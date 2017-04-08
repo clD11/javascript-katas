fc3dd1905463c6edc081832cf7ae33ff79d51129
Kit <kit@fake.com>
2017-04-08 22:32:57 +0100

    Refactor of roman numeral kata

--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -1,52 +1,30 @@
 var convert = (function () {
 
+	var numeralArabicPairs = [
+		["100", "C"],
+		["90", "XC"],
+		["50", "L"],
+		["40", "XL"],
+		["10", "X"],
+		["9", "IX"],
+		["5", "V"],
+		["4", "IV"],
+		["1", "I"]
+	];
+
 	return {
 		numberToRomanNumeral : function (number) {			
-			var numeral = "";			
-			var count = parseInt(number)
-				
-			while (count > 0) {				
-				
-				if (count >= 100) {
-					numeral = numeral.concat("C");
-					count = count - 100;
-				} else if (count >= 90) {
-					numeral = numeral.concat("XC");
-					count = count - 90;
-				} else if (count >= 50) {
-					numeral = numeral.concat("L");
-					count = count - 50;
-				} else if (count >= 40) {
-					numeral = numeral.concat("XL");
-					count = count - 40;					
-				} else if (count >= 10) {
-					numeral = numeral.concat("X");
-					count = count - 10;					
-				} else {
-				
-					if (count >= 9) {
-						numeral = numeral.concat("IX");
-						count = count - 9;
-					}
-				
-					if (count >= 5) {
-						numeral = numeral.concat("V");
-						count = count - 5;
-					}
-					
-					if (count >= 4) {
-						numeral = numeral.concat("IV");
-						count = count - 4;
-					}
-					
-					if (count >= 1) {
-						numeral = numeral.concat("I");
-						count = count - 1;
-					}
+			var numeral = "";
+			var count = parseInt(number);
+
+			numeralArabicPairs.forEach(function(pair) {
+				while (count >= parseInt(pair[0])) {
+					numeral = numeral.concat(pair[1]);
+					count -= pair[0];
 				}
-			}			
+			});
+
 			return numeral;
 		}
 	}
 })();
-
